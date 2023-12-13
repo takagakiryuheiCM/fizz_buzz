408c843544426e658679280ebe24bc35ff3e07cc
takagakiryuheiCM <takagaki.ryuhei@classmethod.jp>
2023-12-13 14:32:16 +0900

    feat: fizz_buzz_list関数を作成

--- app/fizz_buzz.py
+++ app/fizz_buzz.py
@@ -2,7 +2,7 @@ from typing import Literal
 
 def fizz_buzz(number: int) -> str:
     if not 1 <= number <= 100:
-        raise ValueError("1~100の間の数値を指定してください")
+        raise ValueError(f"1~100の間の数値を指定してください。不正な数値: {number}")
 
     is_fizz_num = number % 3 == 0
     is_buzz_num = number % 5 == 0
@@ -15,7 +15,12 @@ def fizz_buzz(number: int) -> str:
         return "Buzz"
     else:
         return str(number)
+    
+def fizz_buzz_list(start: int, end: int) -> list[str]:
+    return [fizz_buzz(n) for n in range(start, end + 1)]
 
 if __name__ == "__main__":
-    for i in range(1, 101):
-        print(fizz_buzz(i))
+    print('\n'.join(fizz_buzz_list(1, 100)))
+
+    # for i in range(1, 101):
+    #     print(fizz_buzz(i))
